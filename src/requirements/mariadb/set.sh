132f03abf4c645307951e9fda31d27f272ca0eed
youskim <youskim@naver.com>
2022-12-07 18:29:13 +0900

    mariadb final

--- src/requirements/mariadb/set.sh
+++ src/requirements/mariadb/set.sh
@@ -3,19 +3,12 @@
 if [ ! -d /var/lib/mysql/$DB_NAME ];
 then
 	service mysql restart
-	echo 1
 	mysql -e "CREATE DATABASE $DB_NAME";
-	echo 2
 	mysql -e "CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PW'";
-	echo 3
 	mysql -e "GRANT ALL ON $DB_NAME.* TO '$DB_USER'@'%'";
-	mysql -e "ALTER USER'root'@'localhost' IDENTIFIED BY '$DB_PW'";
-#	mysql -e "SET PASSWORD FOR 'root'@'localhost'=PASSWORD('$DB_PW')";
-	echo 4
+	mysql -e "SET PASSWORD FOR 'root'@'localhost'=PASSWORD('$DB_PW')";
 	mysqladmin -uroot -p$DB_PW shutdown
-	echo 5
 	exec mysqld_safe
 else
-	echo 6
 	exec mysqld_safe
 fi
